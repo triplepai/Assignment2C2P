725b09e936af4d828490ab9875d7244c9858390a
pawit <triplepai.14@gmail.com>
2019-01-15 10:45:43 +0700

    Add CustomerController and CustomerService template

---
+++ 2C2PWebAPI/2C2PWebAPI/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using _2C2PWebAPI.Models.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace _2C2PWebAPI.Controllers
+{
+    public abstract class BaseController : ApiController
+    {
+        protected Response<T> Response<T>(T data)
+        {
+            return new Response<T>(data);
+        }
+
+        protected Response Response()
+        {
+            return new Response();
+        }
+    }
+}
---
+++ 2C2PWebAPI/2C2PWebAPI/Service/CustomerService.cs
@@ -0,0 +1,12 @@
+﻿using _2C2PWebAPI.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2C2PWebAPI.Service
+{
+    public class CustomerService : ICustomerService
+    {
+    }
+}
