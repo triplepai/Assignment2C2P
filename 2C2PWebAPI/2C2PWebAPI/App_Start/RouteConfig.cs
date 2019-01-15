65bc70c632f0da85b33f5064bda23a46d531bb46
pawit <triplepai.14@gmail.com>
2019-01-15 19:46:52 +0700

    Add swagger and landing page

--- 2C2PWebAPI/2C2PWebAPI/App_Start/RouteConfig.cs
+++ 2C2PWebAPI/2C2PWebAPI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _2C2PWebAPI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller ="Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
